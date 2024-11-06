54b3e91d58b11b06ee28ca47cdddccd99a5af954
Ahamedhsenid <ahamed.a@hsenidlanka.com>
2024-11-06 13:35:53 +0530

    physician login automation validation

---
+++ src/main/java/org/example/pages/DashboardPage.java
@@ -0,0 +1,114 @@
+package org.example.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DashboardPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By employeeList = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/nb-sidebar/div/div/nb-menu/ul/li[2]/a");
+    private By newRegistration = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/div/div/nb-layout-column/div/ngx-employee-list-main/div/div[6]/button");
+    private By language = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/div/nb-layout-header/nav/ngx-header/div[2]/nb-actions/nb-action/div");
+    private By langSelect = By.xpath("//*[contains(text(), 'English')]");
+    private By settings = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/nb-sidebar/div/div/nb-menu/ul/li[14]/a");
+    private By basicInfoSettings = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div[1]/div/div/nb-sidebar/div/div/nb-menu/ul/li[14]/ul/li[3]/a");
+    private By add = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/div/div/nb-layout-column/div/ngx-basic-information-setting/nb-card/nb-card-body/nb-tabset/ul/li[2]/a");
+    private By updateEmployee = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div[1]/div/div/div/div/nb-layout-column/div/ngx-employee-list-main/nb-card/nb-card-body/div/ngx-data-table/div/div[2]/div/div/ng2-smart-table/table/tbody/tr[2]/td[1]/ng2-smart-table-cell/table-cell-view-mode/div/custom-view-component/ngx-table-view-employee/div[3]/label/u");
+    private By employeeEditBtn = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div[1]/div/div/div/div/nb-layout-column/div/ngx-employee-details/nb-card/nb-card-body/div[2]/button");
+    private By accountInfoIcon = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/nb-sidebar/div/div/ngx-toggle-button/div[2]/div/span/img");
+    private By emailAddressField = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/div/div/nb-layout-column/div/ngx-profile-management/div/div/div/nb-card[1]/nb-card-body/div[5]/div[2]/input");
+    private By infoUploadBtn = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div[1]/div/div/div/div/nb-layout-column/div/ngx-employee-list-main/div/div[5]/button");
+
+   //for physician login
+    private By healthCheck = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/nb-sidebar/div/div/nb-menu/ul/li[3]/a/span");
+    private By employeeJudgement = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/nb-sidebar/div/div/nb-menu/ul/li[3]/ul/li/a");
+    private By employeeJudgementTitle = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div/div/div/div/nb-layout-header/nav/ngx-header/div[1]/label");
+    private By healthcheckTitle = By.xpath("/html/body/ngx-app/ngx-pages/ngx-one-column-layout/nb-layout/div[1]/div/div/div/div/nb-layout-column/div/ngx-emp-decision-list/nb-card/nb-card-body/nb-tabset/ul/li[1]/a");
+
+
+
+
+
+
+
+
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+
+
+
+
+    public void setLanguage(){
+        WebElement languageElement = driver.findElement(language);
+        languageElement.click();
+        WebElement languageSelect = driver.findElement(langSelect);
+        languageSelect.click();
+    }
+
+    public void clickSettings(){
+        WebElement settingsElement = driver.findElement(settings);
+        settingsElement.click();
+        WebElement basicSettingsElement = driver.findElement(basicInfoSettings);
+        basicSettingsElement.click();
+    }
+
+    public void clickAdd(){
+        WebElement addElement = driver.findElement(add);
+        addElement.click();
+    }
+
+
+
+    public void clickUpdateEmployee(){
+        WebElement updateElement = driver.findElement(updateEmployee);
+        updateElement.click();
+    }
+
+    public void clickEmployeeEditBtn(){
+        WebElement editElement = wait.until(ExpectedConditions.elementToBeClickable(employeeEditBtn));
+        editElement.click();
+    }
+
+    public void clickProfileIcon(){
+        WebElement iconElement = wait.until(ExpectedConditions.visibilityOfElementLocated(accountInfoIcon));
+        iconElement.click();
+    }
+
+
+
+    public void clickEmployeeInfoUpload(){
+        WebElement uploadElement = driver.findElement(infoUploadBtn);
+        uploadElement.click();
+    }
+
+
+
+    public void clickHealthCheck(){
+        WebElement healthCheckElement =  wait.until(ExpectedConditions.visibilityOfElementLocated(healthCheck));
+        healthCheckElement.click();
+    }
+
+    public void clickEmployeeJudgement(){
+        WebElement employeeJudgementElement =  wait.until(ExpectedConditions.visibilityOfElementLocated(employeeJudgement));
+        employeeJudgementElement.click();
+    }
+
+    public void EmployeeJudgementTitle(){
+        WebElement employeeJudgementTitleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(employeeJudgementTitle));
+        employeeJudgementTitleElement.isDisplayed();
+    }
+    public void healthcheckTitle(){
+        WebElement healthcheckTitleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(healthcheckTitle));
+        healthcheckTitleElement.click();
+    }
+
+}
---
+++ src/test/java/PhysicianTest.java
@@ -0,0 +1,149 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.example.pages.DashboardPage;
+import org.example.pages.LoginPage;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+public class PhysicianTest {
+    private static ExtentReports extentReports;
+    private static ExtentTest extentTest;
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    public static void main(String[] args) {
+        setupExtentReports();
+        setupWebDriver();
+
+        try {
+            extentTest = extentReports.createTest("Physician Login Check Automation");
+
+            driver.get("https://qa.pht.hsenidjapan.com/auth/login");
+            LoginPage loginPage = new LoginPage(driver);
+            DashboardPage dashboardPage = new DashboardPage(driver);
+
+
+            performLogin(loginPage);
+            handleDeviceSwitch(loginPage);
+         //  navigateToSettingsPage(dashboardPage);
+           physicianLogin(dashboardPage, loginPage);
+
+
+
+//            addNewOfficeValidation(officeCreationPage);
+
+        } catch (Exception e) {
+            logFailure("Test failed: " + e.getMessage());
+        } finally {
+            tearDown();
+        }
+    }
+
+    private static void setupExtentReports() {
+        extentReports = new ExtentReports();
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("physician-login-report.html");
+        extentReports.attachReporter(sparkReporter);
+    }
+
+    private static void setupWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    private static void performLogin(LoginPage loginPage) {
+
+        loginPage.setUsername("nimasha@hsenidoutsourcing.com");
+        loginPage.setPassword("123456");
+        loginPage.clickLogin();
+        extentTest.log(Status.PASS, "Login test passed");
+    }
+
+    private static void physicianLogin(DashboardPage dashboardPage, LoginPage loginPage) {
+
+        try {
+            dashboardPage.setLanguage();
+            dashboardPage.clickHealthCheck();
+            dashboardPage.clickEmployeeJudgement();
+            dashboardPage.EmployeeJudgementTitle();
+            dashboardPage.healthcheckTitle();
+
+            String currentUrl = loginPage.getCurrentUrl();
+            if (currentUrl.equals("https://qa.pht.hsenidjapan.com/health-check/decision/list")) {
+                extentTest.pass("Physician Login successful with correct URL: " + currentUrl);
+            } else {
+                extentTest.fail("Physician Login failed, incorrect URL: " + currentUrl);
+            }
+
+        } catch (NoSuchElementException e) {
+            extentTest.log(Status.INFO, "Physician login failed");
+        }
+    }
+
+    private static void handleDeviceSwitch(LoginPage loginPage) {
+        try {
+            loginPage.setSwitchDeviceButtonReg();
+            extentTest.log(Status.INFO, "Handled device switch warning");
+        } catch (NoSuchElementException e) {
+            extentTest.log(Status.INFO, "No device switch warning displayed");
+        }
+    }
+
+    private static void navigateToSettingsPage(DashboardPage dashboardPage) {
+        dashboardPage.setLanguage();
+        dashboardPage.clickSettings();
+        extentTest.log(Status.PASS, "Navigated to settings page");
+
+        dashboardPage.clickAdd();
+        extentTest.log(Status.PASS, "Navigated to Basic Info Add/Edit Page");
+    }
+
+
+
+
+
+    private static void logFailure(String message) {
+        extentTest.log(Status.FAIL, message);
+        takeScreenshot("test-failure.png");
+        try {
+            extentTest.addScreenCaptureFromPath("test-failure.png");
+        } catch (Exception e) {
+            extentTest.log(Status.WARNING, "Failed to attach screenshot: " + e.getMessage());
+        }
+    }
+
+    private static void takeScreenshot(String fileName) {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(scrFile, new File(fileName));
+        } catch (IOException e) {
+            extentTest.log(Status.WARNING, "Failed to capture screenshot: " + e.getMessage());
+        }
+    }
+
+    private static void tearDown() {
+        if (driver != null) {
+//            driver.quit();
+        }
+        extentReports.flush();
+    }
+
+}
+
+
+
